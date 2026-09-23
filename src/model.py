4d51e4c9eb1e35bc6697f09d444b51f315804b9e
Stanley Lo <stanleylo.hk@gmail.com>
2026-09-23 14:52:00 +0800

    add src folder

---
+++ src/model.py
@@ -0,0 +1,52 @@
+import torch
+import torch.nn as nn
+
+
+# CNN architecture: baseline model used for landmark classification
+class MyModel(nn.Module):
+    def __init__(self, num_classes: int = 1000, dropout: float = 0.7) -> None:
+
+        super().__init__()
+
+        # Feature Extractor: [3, 224, 224] -> [512, 7, 7]
+        self.model = nn.Sequential(
+            # Block 1: 224 -> 112
+            nn.Conv2d(3, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+
+            # Block 2: 112 -> 56
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+
+            # Block 3: 56 -> 28
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+
+            # Block 4: 28 -> 14
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+
+            # Block 5: 14 -> 7
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+
+            nn.Flatten(),
+            # 512 channels * 7 * 7 spatial pixels = 25088
+            nn.Linear(512 * 7 * 7, 1024),
+            nn.ReLU(),
+            nn.Dropout(p=dropout),
+            nn.Linear(1024, num_classes)
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.model(x)
